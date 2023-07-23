ed4b50a7fb9f3464feb16671a577a4e7ab2b395e
BradCodeCraft <bradcodecraft@gmail.com>
2023-07-22 22:02:30 -0400

    7/9 Project Application : randomquotemachine

--- Project Application/randomquotemachine/src/App.js
+++ Project Application/randomquotemachine/src/App.js
@@ -1,48 +1,57 @@
-import React from 'react';
-import { FaTwitter } from "react-icons/fa";
+import React, { useState, useEffect} from 'react';
 import './App.css'
 
-class Quote extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            text: 'Life is like throwing a dice; you can\'t control the outcome, but you can control which dice to use',
-            author: 'unknown',
-        };
-        this.requestChange = this.requestChange.bind(this);
-    }
+function App() {
+  const [quotes, setQuotes] = useState('');
 
-    requestChange() {
-        let quoteDict = fetch("https://type.fit/api/quotes");
-        quoteDict.then(response => response.json())
-            .then(data => {
-                let rng = Math.floor(Math.random() * data.length);
-                let dataText = data[rng]['text']
-                let author = (data[rng]['author'] == null) ? "unknown" : data[rng]['author']
-                this.setState({
-                    text: dataText,
-                    author: author
-                });
-            });
-        
-    }
+  const getQuote = () => {
+    fetch("https://type.fit/api/quotes")
+      .then(response => response.json())
+      .then(data => {
+        let rng = Math.floor(Math.random() * data.length);
+        setQuotes(data[rng]);
+      })
+  }
 
-    render() {
-        return (
-            <div className='page'>
-                <div className='container'>
-                    <blockquote id='quote-box'>
-                        <h1 id='text'>{this.state.text}</h1>
-                        <p id='author'>{this.state.author}</p>
-                    </blockquote>
-                    <div className='row'>
-                        <a id='tweet-quote' href='twitter.com/intent/tweet' target='_top'><FaTwitter /></a>
-                        <button className='btn' id='new-quote' onClick={this.requestChange}>New Quote</button>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  return (
+    <div className='app'>
+      <div className='container'>
+        <blockquote
+          id='quote-box'
+        >
+          <p 
+            id='text'
+          >
+            {quotes.text}
+          </p>
+          <p id='author'>
+            - {quotes.author}
+          </p>
+        </blockquote>
+        <span className='btn-container'>
+          <a
+            className='btn'
+            id='tweet-quote'
+            target='_blank'
+            href={`https://twitter.com/intent/tweet?text=${quotes.text}`}
+            rel='noopener noreferrer'
+          >
+            Twitter
+          </a>
+          <button
+            onClick={getQuote}
+            className='btn'
+          >
+            New Quote
+          </button>
+        </span>
+      </div>
+    </div>
+  )
 }
 
-export default Quote;
+export default App;
